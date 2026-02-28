b458ae761524b798c4509c2e5e2acce73ed1941f
Arshad <mohdarshad12575@gmail.com>
2026-02-28 15:37:32 +0530

    calc js logic  working

---
+++ mathematic-calculator/calclogic.js
@@ -0,0 +1,24 @@
+let displayOutput = document.querySelector(".display")
+let expression = ""
+
+let btn = document.querySelectorAll("button");
+
+btn.forEach( (b) => {
+    b.addEventListener("click", (e) => {
+        let trackBtn = e.target.innerHTML
+        if(trackBtn == "="){
+            displayOutput.innerHTML = eval(expression)+" times ily babe"
+            displayOutput.style.fontSize = "1.2rem";
+
+        } else if(trackBtn == "AC") {
+            expression = "";
+            displayOutput.innerHTML = "0";
+        } else if(trackBtn == "DEL") {
+            expression = expression.substring(0,expression.length - 1);
+            displayOutput.innerHTML = expression;
+        } else {
+            expression += trackBtn
+            displayOutput.innerHTML = expression
+        }
+    })
+})
